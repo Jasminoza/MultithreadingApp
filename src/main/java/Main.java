0b65dc0948c2d7dbc7b87a2822489c392694aeb7
Jasminoza <testbot91@gmail.com>
2022-06-03 16:55:08 +0300

    refactored

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,10 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Foo foo = new Foo();
-        Runnable[] runnables = new Runnable[]{
-                () -> foo.first(foo),
-                () -> foo.second(foo),
-                () -> foo.third(foo),
-        };
 
-        Thread foo1Thread = new Thread(runnables[0]);
-        Thread foo2Thread = new Thread(runnables[1]);
-        Thread foo3Thread = new Thread(runnables[2]);
+        Thread foo1Thread = new Thread(() -> foo.first(foo));
+        Thread foo2Thread = new Thread(() -> foo.second(foo));
+        Thread foo3Thread = new Thread(() -> foo.third(foo));
         foo1Thread.start();
         foo1Thread.join();
         foo2Thread.start();
